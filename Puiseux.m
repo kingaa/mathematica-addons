9d4c658e291a04f2f5720e528ec6e19cea1e0b43
Aaron A. King <kingaa@umichedu>
1998-04-25 22:38:32 +0000

    This one works pretty well.
    git-svn-id: svn+ssh://kinglab.eeb.lsa.umich.edu/svn/king/mathematica/trunk/addons@4 f0428215-98c3-464e-b773-cfcd83fbd208

--- Puiseux.m
+++ Puiseux.m
@@ -1,38 +1,54 @@
 (* -*- mode: math; tab-width: 3; -*- *)
 
-BeginPackage["Puiseux`", {"FieldRat`"}]
+BeginPackage["Puiseux`", {"RatSimp`"}]
 
-Unprotect[NewtonPolygon, NewtonPowers, NPSolve]
+Unprotect[NewtonPolygon, Puiseux, RootForm]
 
-NewtonPolygon::usage = "NewtonPolygon[poly, x, y]"
+NewtonPolygon::usage = "NewtonPolygon[poly, x, y] gives the Newton Polygon\
+	of poly(x,y)."
 
-NewtonPowers::usage = "NewtonPowers[poly, x, y]"
+Puiseux::usage = "Puiseux[poly, x, y, order] solves the polynomial equation\
+	poly(x,y) = 0 for y in terms of x by the method of Newton and Puiseux."
 
-NPSolve::usage = "NPSolve[poly, x, y, order]"
+Options[Puiseux] = {RootForm -> True}
 
 Begin["Private`"];
 
-NPRels = {};
-NPPars = {};
+NPRels = {}
+NPPars = {}
 
 NewtonPolygon[p_, x_, y_] := Module[
-	{found, L}, 
-	L = Map[List[#,Exponent[Coefficient[p,x,#],y,Min]] &,Exponent[p,x,List]];
-	L = Sort[L,#1[[1]] < #2[[1]] &];
-	While[ Length[L] > 2, (
-	    For[i=2, i <= Length[L]-1, i++,
-		( found = False;
-		If[(L[[i,2]]-L[[i-1,2]])*(L[[i+1,1]]-L[[i,1]])
-			>= (L[[i+1,2]]-L[[i,2]])*(L[[i,1]]-L[[i-1,1]]), 
-		(found = True; L = Delete[L,i])])
-		 ];
-	    If[ L[[Length[L],2]] == L[[Length[L]-1,2]], 
-		(found = True; L = Delete[L,Length[L]])];
-	    If[ !found, Return[L] ]);
+	{found = False, L}, 
+	L = {#, Exponent[Coefficient[p,x,#],y,Min]}& /@ Union[Exponent[p,x,List]];
+	L = Sort[L, (#1[[1]] < #2[[1]])&];
+	For[ j = 2, j <= Length[L], j++,
+		If[ L[[j,2]] == 0, 
+			found = True;
+			Break[ ]
+		]
+	];
+	If[found, L = Take[L,j]];
+	While[ Length[L] > 2,
+		For[ i = 2, i <= Length[L]-1, i++,
+			found = False;
+			If[ (L[[i,2]]-L[[i-1,2]])*(L[[i+1,1]]-L[[i,1]])
+				>= (L[[i+1,2]]-L[[i,2]])*(L[[i,1]]-L[[i-1,1]]), 
+				found = True; 
+				L = Delete[L,i]
+			];
+		];
+		If[ L[[Length[L],2]] == L[[Length[L]-1,2]], 
+			found = True; 
+			L = Delete[L,Length[L]]
+		];
+		If[ !found, 
+			Return[ L ]
+		]
 	];
 	If[ (Length[L] == 2) && (L[[1,2]] == L[[2,2]]),
-		L = {L[[1]]} ];
-	Return[L]; 
+		L = {L[[1]]}
+	];
+	Return[ L ]
 ]
 
 NewtonPowers[p_, x_, y_] := Module[
@@ -52,31 +68,51 @@ NPBlowup[p_, x_, y_] := Map[Module[
 	NPSubsts[p, x, y]
 ]
 
-PolySimp[f_] := FieldRat[f, NPRels, NPPars]
-
 NPShift[p_, x_, y_, n_Integer] := Module[
-	{param, sol, s, q, r = {}, i},
-	sol = (p /. x->0);
-	sol = FactorList[( sol / (y^Exponent[sol, y, Min]) //Expand)];
-	For[i = 2, i <= Length[sol], i++,
-		If[ ! NumberQ[sol[[i,1]]],
-			If [Exponent[sol[[i,1]], y] == 1, 
-				param = Part[Solve[sol[[i,1]]==0, y], 1, 1, 2],
-				param = Unique["k"];
-				NPPars = Prepend[NPPars, param];
-				NPRels = GroebnerBasis[
-					Append[
-						NPRels,
-						(sol[[i,1]] /. y->param)
-					], 
-					NPPars
-				]
-			];
+	{param, sol, m, s, q, r = {}, i},
+	sol = #[[1]]& /@ Rest[FactorList[ p /. x -> 0 ]];
+	For[i = 1, i <= Length[sol], i++,
+		m = Exponent[sol[[i]], y];		
+		If[ m == 1,
+			param = Solve[sol[[i]]==0, y][[1,1,2]];
 			s = y -> y + param;
-			q = PolySimp[(p /. s) //Expand];
-			If [ Expand[q /. {x->0, y->0}] == 0,
-				r = Append[r, NPS[s, NPResolution[q,x,y,n-1]]];
+			q = Expand[p /. s];
+		];
+		If[ m > 1,
+			param = Unique["k"];
+			NPPars = Prepend[NPPars, param];
+			NPRels = Append[NPRels, (sol[[i]] /. y->param)]; 
+			s = y -> y + param;
+			q = RatSimp[ Expand[p /. s], NPRels, NPPars ] //Expand;
+		];
+		If[ TrueQ[Expand[q /. {x -> 0, y -> 0}] == 0], 
+			If[ !TrueQ[ param == 0 ],
+				r = Append[r, NPS[s, NPResolution[q,x,y,n-1]]]
 			]
+		]
+	];
+	Return[ r ]
+]
+
+NPSolve[p_, x_, y_, n_Integer] := Module[
+	{param, sol, m, s, q, r = {}, i},
+	sol = #[[1]]& /@ Rest[FactorList[ p /. x -> 0 ]];
+	For[i = 1, i <= Length[sol], i++,
+		m = Exponent[sol[[i]], y];		
+		If[ m == 1,
+			param = Solve[sol[[i]]==0, y][[1,1,2]];
+			s = y -> y + param;
+			q = Expand[p /. s];
+		];
+		If[ m > 1,
+			param = Unique["k"];
+			NPPars = Prepend[NPPars, param];
+			NPRels = Append[NPRels, (sol[[i]] /. y->param)];
+			s = y -> y + param;
+			q = RatSimp[ Expand[p /. s], NPRels, NPPars ] //Expand;
+		];
+		If[ TrueQ[Expand[q /. {x -> 0, y -> 0}] == 0], 
+			r = Append[r, NPS[s, NPResolution[q,x,y,n]]]
 		]
 	];
 	Return[ r ]
@@ -95,32 +131,20 @@ NPResolution[p_, x_, y_, n_Integer/;(n > 0)] := Module[
 	]
 ]	
 
-UndetCoeff[p_, x_, y_, n_Integer] := Module[
-	{u, s, q, sols, ss},
-	s = Sum[u[k] x^k, {k,1,n}],
- 	q = Normal[Series[(p /. y -> s),{x,0,n}]];
-	sols = Solve[Table[Coefficient[q,x,k]==0,{k,1,n}],
-		Table[u[k],{k,1,n}]];
-	ss = Map[PolySimp[s /. #] &, sols];
-	ss = Map[(y -> #) &, Together[ss]];
-	Return[ Apply[UDC, ss] ]
-]
+NPBackSolve[_, {}, _] := {}
 
 NPBackSolve[e_, s_, y_] := Module[
 	{n = Length[s], k},
-	If [ n == 0, Return[ { e /. y -> 0 } ] ];
-	If [ Head[s] == NPBottom,
+	If[ n == 0, Return[ { e /. y -> 0 } ] ];
+	If[ Head[s] == NPBottom,
 		Return[ { e /. s[[1]] } ]
 	];
-	If [ Head[s] == UDC,
-		Return[ Table[(e /. s[[k]]), {k,1,n}] ]
-	];
-	If [ Head[s] == NPS,
+	If[ Head[s] == NPS,
 		Return[
 			NPBackSolve[(e /. s[[1]]), s[[2]], y]
 		]
 	];
-	If [ Head[s] == NPB,
+	If[ Head[s] == NPB,
 		Return[
 			Join @@ Table[
 				NPBackSolve[(e /. s[[1]]), s[[k]], y],
@@ -128,7 +152,7 @@ NPBackSolve[e_, s_, y_] := Module[
 			]
 		]
 	];
-	If [ Head[s] == List,
+	If[ Head[s] == List,
 		Return[
 			Join @@ Table[
 				NPBackSolve[e, s[[k]], y],
@@ -138,28 +162,28 @@ NPBackSolve[e_, s_, y_] := Module[
 	];
 ]
 
-NPSolve[p_, x_, y_, n_Integer] := Module[
-	{q = FactorList[p], F, rels, r, s, i},
+Puiseux[p_, x_, y_, n_Integer, opts___] := Module[
+	{q = Rest[FactorList[Expand[p / (x^Exponent[p,x,Min])]]], 
+		F, r, s, i, exp},
  	NPPars = {};
 	NPRels = {};
-	s = Table[ NPResolution[q[[i,1]], x, y, n], {i,2,Length[q]}];
-	s = s /. UDC[s_] -> UDC[s, x, y, n];
-	s = s /. UDC -> UndetCoeff;
+	Off[Solve::svars];
+	s = Table[ NPResolution[q[[i,1]], x, y, n], {i,1,Length[q]}];
 	r = NPBackSolve[F[x,y], s, y];
 	m = Map[Exponent[First[#], x]&, r];
-	Return[
-		{
-			Table[
-				(r[[k,2]] /. x->x^(1/m[[k]]) //Expand),
+	exp = Table[
+				RatSimp[ Expand[r[[k,2]] /. x -> x^(1/m[[k]])], NPRels, NPPars] //Expand,
 				{k,1,Length[r]}
-			],
-			 NPRels
-		}
-	] 
+			];
+	On[Solve::svars];
+	If[ RootForm /. {opts} /. Options[Puiseux],
+		Union @@ ((exp /. #)& /@ Solve[ Thread[NPRels == 0], NPPars]),
+		{exp, NPRels}
+	]
 ]
 
 End[ ]
 
-Protect[NewtonPolygon, NewtonPowers, NPSolve]
+Protect[NewtonPolygon, Puiseux, RootForm]
 
 EndPackage[ ]
