02f5a735392ce5f8cacb6b5066f9026b7a014f01
Aaron A. King <kingaa@umichedu>
1998-11-30 23:11:40 +0000

    Initial revision
    git-svn-id: svn+ssh://kinglab.eeb.lsa.umich.edu/svn/king/mathematica/trunk/addons@17 f0428215-98c3-464e-b773-cfcd83fbd208

---
+++ init.m
@@ -0,0 +1,31 @@
+SetOptions[$Output,PageWidth -> 110]
+DeclarePackage["Frechet`", {"Frechet"}]
+DeclarePackage["Tr`", {"Tr"}]
+DeclarePackage["Taylor`", {"Taylor", "TaylorCoeff", "TotalDegree", "InitialForm"}] 
+DeclarePackage["RatSimp`", {"RatSimp"}] 
+DeclarePackage["Puiseux`", {"Puiseux", "NewtonPolygon"}] 
+DeclarePackage["NormalForm`", {"NormalForm", "ForwardAdjointAction", 
+	"BackwardAdjointAction", "FunctionNormalForm", "ForwardAction", 
+	"BackwardAction", "Jordan", "VFTransform", "Complexification", 
+	"Realification", "Expon", "LieBracket", "Generator"}]
+DeclarePackage["Ideal`", {"MultiIndices", "Monomials", "IdealSum",
+	"IdealProduct", "IdealIntersection", "Saturation", "Homogeneize", 
+	"Dehomogeneize", "GeneralPolynomial", "StandardBasis",
+	"TangentCone", "BlockDiagonal", "GrevLex", "GrLex", "Lex"}]
+DeclarePackage["NewtonPolyhedron`", {"NewtonPolyhedron", "Support"}]
+DeclarePackage["WriteBin`", {"WriteBin"}]
+DeclarePackage["Dlivr`", {"Dlivr"}]
+DeclarePackage["BifCurve`", {"BifCurve", "FindTangent", "NewtRaph", "Funcv"}]
+DeclarePackage["Graphics`ImplicitPlot`", {"ImplicitPlot"}]
+DeclarePackage["NumericalMath`Horner`", {"Horner"}]
+DeclarePackage["Algebra`SymmetricPolynomials`", {"SymmetricPolynomial", 
+	"SymmetricReduction"}]
+DeclarePackage["LinearAlgebra`Orthogonalization`", {"Normalize", "Projection",
+	"GramSchmidt"}]
+
+If[Environment["MATHINDENTCOOKIE"] =!= $Failed,
+	   $BatchInput=False;
+	   If[NameQ["System`Private`$IndentSuffix"],
+	     ToExpression[
+	       "System`Private`$IndentSuffix = Environment[\"MATHINDENTCOOKIE\"]"];
+	     Print[" ", Environment["MATHINDENTCOOKIEMSG"]]]]
