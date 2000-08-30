39d1d6f7004122c4b0848185539d9c41d26a2ada
Aaron A. King <kingaa@umichedu>
2000-08-30 19:59:33 +0000

    Removed some packages. Changed Expon to Exponential
    git-svn-id: svn+ssh://kinglab.eeb.lsa.umich.edu/svn/king/mathematica/trunk/addons@29 f0428215-98c3-464e-b773-cfcd83fbd208

--- init.m
+++ init.m
@@ -3,20 +3,16 @@ DeclarePackage["Frechet`", {"Frechet"}]
 DeclarePackage["Tr`", {"Tr"}]
 DeclarePackage["Taylor`", {"Taylor", "TaylorCoeff", "TotalDegree", 
 	"InitialForm", "TaylorCompress"}] 
-DeclarePackage["RatSimp`", {"RatSimp"}] 
-DeclarePackage["Puiseux`", {"Puiseux", "NewtonPolygon"}] 
 DeclarePackage["NormalForm`", {"NormalForm", "ForwardAdjointAction", 
 	"BackwardAdjointAction", "FunctionNormalForm", "ForwardAction", 
 	"BackwardAction", "Jordan", "VFTransform", "Complexification", 
-	"Realification", "Expon", "LieBracket", "Generator"}]
+	"Realification", "Exponential", "LieBracket", "Generator"}]
 DeclarePackage["Ideal`", {"MultiIndices", "Monomials", "IdealSum",
 	"IdealProduct", "IdealIntersection", "Saturation", "Homogeneize", 
 	"Dehomogeneize", "GeneralPolynomial", "StandardBasis",
 	"TangentCone", "BlockDiagonal", "GrevLex", "GrLex", "Lex"}]
-DeclarePackage["NewtonPolyhedron`", {"NewtonPolyhedron", "Support"}]
 DeclarePackage["WriteBin`", {"WriteBin"}]
 DeclarePackage["Dlivr`", {"Dlivr"}]
-DeclarePackage["BifCurve`", {"BifCurve", "FindTangent", "NewtRaph", "Funcv"}]
 DeclarePackage["Graphics`ImplicitPlot`", {"ImplicitPlot"}]
 DeclarePackage["NumericalMath`Horner`", {"Horner"}]
 DeclarePackage["Algebra`SymmetricPolynomials`", {"SymmetricPolynomial", 
